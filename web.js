43cb132581be1e3a88248a3b817cdd7af1dad2ed
Tom Berman <tjcberman@gmail.com>
2013-03-15 22:18:25 +0000

    readMongoDB_2

--- web.js
+++ web.js
@@ -1,7 +1,6 @@
 var express = require('express');
-var mongo = require('mongoskin');
 
-
+var request = require("request");
 
 
 
@@ -15,7 +14,7 @@ app.get('/', function(request, response) {
 });
 
 app.get('/readMongoDB', function(request, response) {
-var request = require("request");
+
 
 request('https://api.mongolab.com/api/1/databases/heroku_app13153679/collections?apiKey=TS3DbEeRXGUBrrX3It--_vLWIzZN5SB2', function(error, response, body) {
 
@@ -27,27 +26,7 @@ res.end(body);
 
 });
 
-app.get('/read', function(request, response) {
-  
-  var rslt = ' R : ';
 
-   var db = require('mongoskin').db('localhost:27017/test');
- 
- db.collection('things').find().toArray(function(err, result) {
-    if (err) throw err;
-    rslt += result;
-    console.log(result);
-});
-
-response.send('results ' + rslt);
-
-});
-
-app.get('/write', function(request, response) {
-  response.send('write junck to db');
-  
-  
-});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
