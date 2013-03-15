c3691472d7e0f1580ff9a3e213aaf3f8e1e15b4f
Tom Berman <tjcberman@gmail.com>
2013-03-15 23:05:05 +0000

    readded request again

--- web.js
+++ web.js
@@ -13,6 +13,17 @@ app.get('/', function(request, res) {
   res.send('Hello Andy');
 });
 
+app.get('/read', function(request, res) {
+  
+  var purl = 'https://api.mongolab.com/api/1/databases/heroku_app13153679/collections?apiKey=TS3DbEeRXGUBrrX3It--_vLWIzZN5SB2';
+  request(purl, function(error, response, body) {
+
+res.end(body);
+
+
+});
+  
+});
 
 
 
