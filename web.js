feff9320a3e4f156e5b7f164baf16ae36985c5bb
Andy Armstrong <andyarmstrong@hotmail.co.uk>
2013-04-10 22:03:43 +0100

    dasda

--- web.js
+++ web.js
@@ -37,6 +37,12 @@ res.end(body);
 
 app.get('/s3test', function(request,res){
 
+
+amazonS3.ListBuckets(function(err, data) {
+    console.log(err);
+    console.log(data);
+});
+
 storage.PutObject({
   BucketName:    process.env.BUCKET_NAME,
   ObjectName:    'helloworld.txt',
